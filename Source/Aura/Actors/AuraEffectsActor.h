e8ac545a3bf5c8e3533e4c24e1daf4f48a79a6e5
Spoiler335 <sushantsai8@gmail.com>
2025-08-06 16:31:08 -0400

    Setup Duration Gameplay Effects

--- Source/Aura/Actors/AuraEffectsActor.h
+++ Source/Aura/Actors/AuraEffectsActor.h
@@ -24,4 +24,7 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly ,Category="Applied Effects")
 	TSubclassOf<UGameplayEffect> instantGamePlayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly ,Category="Applied Effects")
+	TSubclassOf<UGameplayEffect> durationGamePlayEffectClass;
 };
